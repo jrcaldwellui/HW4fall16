9846728f018226adf201f850f3b7aefa9d9567c5
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-26 21:59:01 +0000

    ez fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     
     class << self
         def create_user!(params)
+            puts("User id of new user: #{params.user_id}")
             if( User.find_by user_id: params.user_id )
                 return nil
             else
