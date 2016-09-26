821f69d26eb2b479b770f44d13292cea55792772
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-26 00:48:20 +0000

    add new and create to user contoler and new view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    #this will create new user and check for duplicate user id
+  end
+ end
