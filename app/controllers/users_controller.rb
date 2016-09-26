9e74ca74886a20a2834330eda42362222e5b6da4
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-26 20:11:08 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create()
-    @user.email = user_params[:email]
-    @user.user_id = user_params[:user_id]
-    puts(@user)
-    @user.save
-    flash[:notice] = "#{@user.user_id} was successfully created."
+    #@user.email = user_params[:email]
+    #@user.user_id = user_params[:user_id]
+    #puts(@user)
+    #@user.save
+    #flash[:notice] = "#{@user.user_id} was successfully created."
     redirect_to movies_path
     #this will create new user and check for duplicate user id
   end
