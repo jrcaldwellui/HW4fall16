7ae31731e9de61881c1d77d175d183cd5b760576
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-27 01:49:20 +0000

    session work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_current_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  
+  def set_current_user
+    @current_user ||=	session[:session_token	] && User.find_by_session_token(session[:session_token])
+    if @current_user
+      puts("user session active")
+    end
+    
+  end
 end
