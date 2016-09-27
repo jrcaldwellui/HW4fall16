8e5683d3469420d473322cd4b475268032312b52
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-27 01:42:49 +0000

    session work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,10 @@ class SessionsController < ApplicationController
         if (user)
             session[:session_token] = user.session_token
             flash[:notice] = "You are logged in as #{user.user_id}"
+            redirect_to movies_path
         else
             flash[:notice] = 'Invalid user-id/email combination'
+            redirect_to login_path
         end
             
    end
