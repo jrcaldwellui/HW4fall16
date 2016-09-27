ba6a9f8a2557f20c3f6d908b83adb6c75d2eff93
James Caldwell <james-r-caldwell@uiowa.edu>
2016-09-27 01:54:30 +0000

    session work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   
   def set_current_user
-    puts("Checking for session token")
+    puts("Checking for session token: #{session[:session_token]}")
     @current_user ||=	session[:session_token	] && User.find_by_session_token(session[:session_token])
     if @current_user
       puts("user session active")
